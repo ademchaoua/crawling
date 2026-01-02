b2bf7397a354a3458714f1e2c8514fd7ce72cdc1
Adem Chaoua <adem.chaoua.1444@gmail.com>
2026-01-02 23:44:28 +0100

    refactor: export getHtmlPage function for external usage

--- src/core/processer.js
+++ src/core/processer.js
@@ -4,7 +4,7 @@ import { fetch } from 'undici';
 import { parentPort } from 'worker_threads';
 import { fileLog, fileErrorLog } from '../logger/index.js';
 
-async function getHtmlPage(url) {
+export async function getHtmlPage(url) {
     const response = await fetch(url, {
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
